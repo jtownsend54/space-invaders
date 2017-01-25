c2e1446a35e46c7cfda1e9cd072c9b85f02302b8
Jon Townsend <jon.townsend@limelyte.com>
2017-01-25 12:33:49 -0800

    Basic Menu and lateral ship movement

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+	public void LoadLevel(string name) {
+		Debug.Log("Load the level: " + name);
+//		Brick.breakableCount = 0;
+		Application.LoadLevel (name);
+	}
+
+	public void LoadNextLevel() {
+//		Brick.breakableCount = 0;
+		Application.LoadLevel(Application.loadedLevel + 1);
+	}
+
+	public void QuitRequest() {
+		Debug.Log("Quit request.");
+		Application.Quit();
+	}
+
+//	public void BrickDestroyed() {
+//		// Guarding against some case where it could go under 0
+//		if (Brick.breakableCount <= 0) {
+//			LoadNextLevel();
+//		}
+//	}
+}
---
+++ Assets/Scripts/ShipController.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShipController : MonoBehaviour {
+	public float speed = 5.0f;
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey (KeyCode.LeftArrow)) {
+			gameObject.transform.position += new Vector3(-speed * Time.deltaTime, 0, 0);
+		} else if (Input.GetKey (KeyCode.RightArrow)) {
+			gameObject.transform.position += new Vector3(speed * Time.deltaTime, 0, 0);
+		}
+	}
+}
