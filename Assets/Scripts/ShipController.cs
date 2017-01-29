1d2b6613c3685fa855b0347b4e758ee165ee2791
Jon Townsend <jon.townsend@limelyte.com>
2017-01-28 17:04:05 -0800

    Restrict movement inside of the screen canvas

--- Assets/Scripts/ShipController.cs
+++ Assets/Scripts/ShipController.cs
@@ -3,13 +3,30 @@ using System.Collections;
 
 public class ShipController : MonoBehaviour {
 	public float speed = 5.0f;
+//	private Camera camera;
+	private Vector3 minMaxX;
+	private Vector3 minMaxY;
+	private float padding = 0.5f;
+
+	void Start() {
+		minMaxX = Camera.main.ViewportToWorldPoint(new Vector3(0, 0, 0));
+		minMaxY = Camera.main.ViewportToWorldPoint(new Vector3(1, 1, 0));
+	}
 
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetKey (KeyCode.LeftArrow)) {
-			gameObject.transform.position += new Vector3(-speed * Time.deltaTime, 0, 0);
+			transform.position += new Vector3(-speed * Time.deltaTime, 0, 0);
 		} else if (Input.GetKey (KeyCode.RightArrow)) {
-			gameObject.transform.position += new Vector3(speed * Time.deltaTime, 0, 0);
+			transform.position += new Vector3(speed * Time.deltaTime, 0, 0);
+		} else if (Input.GetKey (KeyCode.UpArrow)) {
+			transform.position += new Vector3(0, speed * Time.deltaTime, 0);
+		} else if (Input.GetKey (KeyCode.DownArrow)) {
+			transform.position += new Vector3(0, -speed * Time.deltaTime, 0);
 		}
+
+		float xClamp = Mathf.Clamp (transform.position.x, minMaxX.x + padding, minMaxY.x - padding);
+		float yClamp = Mathf.Clamp (transform.position.y, minMaxX.y + padding, minMaxY.y - padding);
+		transform.position = new Vector3 (xClamp, yClamp, 0);
 	}
 }
