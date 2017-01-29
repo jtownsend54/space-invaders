7192887ca43df8d6a3c12fef2dd064cfde2e0099
Jon Townsend <jon.townsend@limelyte.com>
2017-01-28 18:21:28 -0800

    Added enemy prefab and generator prefab

---
+++ Assets/Scripts/EnemyGenerator.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyGenerator : MonoBehaviour {
+	public GameObject enemyPrefab;
+
+	void OnDrawGizmos() {
+		Gizmos.DrawWireSphere (transform.position, 0.5f);
+	}
+
+	// Use this for initialization
+	void Start () {
+		GameObject enemy = Instantiate(enemyPrefab, new Vector3 (transform.position.x, transform.position.y, 0), Quaternion.identity) as GameObject;
+		enemy.transform.position = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
